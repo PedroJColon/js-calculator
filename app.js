cdd2db5e7ad4ac0a96d5aefe78b46aeddb52d5f6
Pedro J Colon <64868232+PedroJColon@users.noreply.github.com>
2025-05-21 18:52:47 -0400

    Add operate function to determine what operations to be done

--- app.js
+++ app.js
@@ -18,6 +18,23 @@ function divide(a,b) {
 	return a / b;
 }
 
+
+function operate() {
+	if (calcuations.operator == operations[0]) {
+		add(calcuations.num1, calcuations.num2);
+	}
+	if (calcuations.operator == operations[1]) {
+		subtract(calcuations.num1, calcuations.num2);
+	}
+	if (calcuations.operator == operations[2]) {
+		multiply(calcuations.num1, calcuations.num2);
+	}
+	if (calcuations.operator == operations[3]) {
+		divide(calcuations.num1, calcuations.num2);
+	}
+	
+}
+
 console.log(add(5,4));
 console.log(subtract(5,4));
 console.log(multiply(5,4));
